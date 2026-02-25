7f1c7f9054fdc46fa0e0a7b4ee681ce4942b358b
TheMrz-01 <alanarapoglu@gmail.com>
2026-02-25 18:54:25 +0300

    Lil ui update

--- focus/focus.js
+++ focus/focus.js
@@ -1,9 +1,9 @@
 const phrases = [
-    "yes i want to waste my time",
-    "i choose distraction over progress",
-    "i am avoiding meaningful work",
-    "i accept that this does not help my goals",
-    "this is not aligned with my future self"
+    " yes i want to waste my time ",
+    " i choose distraction over progress ",
+    " i am avoiding meaningful work ",
+    " i accept that this does not help my goals ",
+    " this is not aligned with my future self "
 ];
 
 const params = new URLSearchParams(window.location.search);
@@ -11,7 +11,6 @@ const target = params.get("target");
 
 const challengeElement = document.querySelector(".container #challenge");
 const input = document.querySelector(".container #input");
-const button = document.querySelector(".container #submit");
 
 const phrase = phrases[Math.floor(Math.random() * phrases.length)];
 challengeElement.textContent = `"${phrase}"`;
@@ -19,8 +18,8 @@ challengeElement.textContent = `"${phrase}"`;
 input.addEventListener("paste", e => e.preventDefault());
 input.addEventListener("contextmenu", e => e.preventDefault());
 
-button.addEventListener("click", async () => {
-    if (input.value.trim() === phrase) {
+input.addEventListener("keydown", async (event) => {
+    if (input.value.trim() === phrase && event.key === "Enter") {
 
         const data = await chrome.storage.local.get("unlockDurationMs");
         const durationMs = data.unlockDurationMs ?? 300000;
@@ -32,7 +31,7 @@ button.addEventListener("click", async () => {
         // Redirect immediately
         window.location.href = target;
 
-    } else {
+    } else if(input.values.trim() !== phrase){
         alert("Incorrect. Try again.");
     }
 });
